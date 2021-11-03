f0aa3dcff8b1fce3fa01fc5cf1e8a875cfd58521
gtcolling <gtcolling@hotmail.com>
2021-11-03 13:15:48 +0000

    added 3 dependencies t hello.py

--- hello.py
+++ hello.py
@@ -1,1 +1,7 @@
-print("hello world")
+import emoji
+import pyjokes
+import numpy
+
+print(emoji.emojize("Python is :thumbs_up:"))
+print(pyjokes.get_joke())
+print(numpy.random.randint(5, size=(2, 4)))
